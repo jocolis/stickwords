8b5bfc630685e4334dbbcc8122ccaa17b5ca13f0
jocolis <jiaji.wu@me.com>
2026-05-31 17:06:48 +0800

    Update server URL placeholder in secrets.example.h

--- firmware/include/secrets.example.h
+++ firmware/include/secrets.example.h
@@ -2,4 +2,4 @@
 
 #define STICKWORDS_WIFI_SSID "your-2.4ghz-wifi-name"
 #define STICKWORDS_WIFI_PASSWORD "your-wifi-password"
-#define STICKWORDS_SERVER_URL "http://192.168.1.100:8000"
+#define STICKWORDS_SERVER_URL "http://192.168.xx.xx:8000"
